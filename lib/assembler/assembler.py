6ec6f16ad1efd3ebedd2cc90016c6552dfb9a917
DimitarYordanov17 <ivoj_louie@protonmail.com>
2021-05-03 19:02:22 +0300

    add library files
    Add assembler, vm translator and front end translator. All these files were first implemented in DimitarYordanov17/book-work/the-elements-of-computing-systems GitHub repository.

---
+++ lib/assembler/assembler.py
@@ -0,0 +1,140 @@
+# A Python assembler for the Hack machine language. @DimitarYordanov17
+# To run: python3 assembler.py {your .asm file}
+
+from assemblerLibrary import AssemblerLibrary
+import os
+import sys
+
+class Assembler:
+  '''
+  Main assembler class, several functions available, note that the input file should first be cleaned, symbolically preprocessed and finally translated
+  '''
+
+  def assemble(path: str):
+    '''
+    Clean, apply symbolic preprocessing and translate a file
+    '''
+    
+    intermediate_file = "program_intermediate.asm"
+    os.system(f"cp {path} {intermediate_file}")
+
+    output_file = path.split('.')[0] + ".hack"
+
+    Assembler.clean(intermediate_file)
+
+    Assembler.symbolic_preprocessor(intermediate_file)
+    
+    Assembler.translate(intermediate_file, output_file)
+
+    os.system(f"rm {intermediate_file}")
+
+  def symbolic_preprocessor(input_file):
+    '''
+    1. Construct a jump and variables symbolic table
+    2. Translate via the symbolic table
+    '''
+    symbolic_table = dict()
+    variables = 0
+
+    with open(input_file, "r+") as f:
+      lines = f.readlines()
+      
+      # Build jump symbolic table part
+      for index, line in enumerate(lines):
+        if line[0] == "(":
+          symbolic_table[line[1:-2]] = index - len(symbolic_table.keys())
+
+      # Build registers symbolic table part
+      for line in lines:
+        if "@" in line:
+          address = line[1:].strip()
+
+          if (not address.isnumeric()) and (address not in symbolic_table.keys()):
+            address_request = AssemblerLibrary.get_register(address)
+
+            if address_request == "VARIABLE":
+              symbolic_table[address] = 16 + variables
+              variables += 1
+      
+      f.seek(0)
+        
+      for line in lines:
+        if line[0] == "@" and not line[1:].strip().isnumeric():
+          address = line[1:].strip()
+
+          if address in symbolic_table.keys():
+            bytecode = symbolic_table[address]
+          else:
+            bytecode = AssemblerLibrary.get_register(address)
+          
+          f.write("@" + str(bytecode) + "\n")
+
+        elif line[0] != "(":
+          f.write(line)
+
+      f.truncate()
+
+  def clean(input_file):
+    '''
+    Remove unnecesary whitespaces and comments
+    '''
+
+    with open(input_file, "r+") as f:
+      lines = f.readlines()
+      f.seek(0)
+      for line in lines:
+        spaceless_line = line.replace(" ", "").lstrip()
+
+        if line != '\n':
+            if "/" in spaceless_line:
+              divided_line = spaceless_line.split("/")
+              if divided_line[0] and divided_line[0] != "*":
+                f.write(divided_line[0] + '\n')
+
+            elif "*" not in spaceless_line:
+              f.write(spaceless_line)
+              
+      f.truncate()
+
+  def translate(input_file, output_file):
+    '''
+    Translate a prepared file
+    '''
+
+    with open(input_file, "r+") as f, open(output_file, 'w') as of:
+      lines = f.readlines()
+      
+      of.seek(0)
+      
+      for line in lines:
+        line = line.strip()
+         
+        instruction_type = '0' if "@" in line else '1'
+
+        if instruction_type == '0':
+          address = line[1:]
+          binary_code = f"{int(address):015b}"
+          machine_code = instruction_type + binary_code
+
+        else:
+          if "=" in line:
+            splitted_instruction = line.split('=')
+            destination = splitted_instruction[0]
+            computation = splitted_instruction[1]
+            jump = ""
+          else:
+            splitted_instruction = line.split(';')
+            computation = splitted_instruction[0]
+            jump = splitted_instruction[1]
+            destination = ""
+
+          machine_code_destination = AssemblerLibrary.get_destination(destination)
+          machine_code_jump = AssemblerLibrary.get_jump(jump)
+          machine_code_computation = AssemblerLibrary.get_computation(computation)
+
+          machine_code = instruction_type + "11" + machine_code_computation + machine_code_destination + machine_code_jump 
+        
+        of.write(machine_code + '\n')
+
+input_file = sys.argv[1]
+Assembler.assemble(input_file)
---
+++ lib/assembler/assemblerLibrary.py
@@ -0,0 +1,103 @@
+# A library file to include the machine language and dictionariy specifications of the Hack language. @DimitarYordanov17
+
+class AssemblerLibrary:
+  '''
+  Main class to map the Hack syntax to internal machine language bytecode
+  '''
+
+  def get_jump(jump: str):
+    '''
+    Return bytecode of jump commands
+    '''
+    bytecode =  {
+      ''   : '000',
+      'JGT': '001',
+      'JEQ': '010',
+      'JGE': '011',
+      'JLT': '100',
+      'JNE': '101',
+      'JLE': '110',
+      'JMP': '111',
+    }
+
+    return bytecode[jump]
+
+  def get_destination(destination: str):
+    '''
+    Return bytecode of destination commands
+    '''
+    bytecode = {
+      ''   : '000',
+      'M'  : '001',
+      'D'  : '010',
+      'MD' : '011',
+      'A'  : '100',
+      'AM' : '101',
+      'AD' : '110',
+      'AMD': '111',
+    }
+
+    return bytecode[destination]
+
+  def get_computation(computation):
+    '''
+    Return bytecode of computation commands
+    '''
+    bytecode = {
+      '0'  : '0101010',
+      '1'  : '0111111',
+      '-1' : '0111010',
+      'D'  : '0001100',
+      'A'  : '0110000',
+      '!D' : '0001101',
+      '!A' : '0110001',
+      '-D' : '0001111',
+      '-A' : '0110011',
+      'D+1': '0011111',
+      'A+1': '0110111',
+      'D-1': '0001110',
+      'A-1': '0110010',
+      'D+A': '0000010',
+      'D-A': '0010011',
+      'A-D': '0000111',
+      'D&A': '0000000',
+      'D|A': '0010101',
+      'M'  : '1110000',
+      '!M' : '1110001',
+      '-M' : '1110011',
+      'M+1': '1110111',
+      'M-1': '1110010',
+      'D+M': '1000010',
+      'D-M': '1010011',
+      'M-D': '1000111',
+      'D&M': '1000000',
+      'D|M': '1010101',
+    }
+
+    return bytecode[computation]
+
+  def get_register(register):
+    '''
+    Return bytecode of built-in registers
+    '''
+    bytecode_mnemonics = {
+      'SP'    : 0,
+      'LCL'   : 1,
+      'ARG'   : 2,
+      'THIS'  : 3,
+      'THAT'  : 4,
+      'SCREEN': 0x4000,
+      'KBD'   : 0x6000,
+    }
+    
+    if register in ['R' + str(n) for n in range(0, 16)]:
+      if len(register) == 2:
+        return int(register[1])
+      
+      return int(register[1] + register[2])
+
+    elif register in bytecode_mnemonics.keys():
+      return bytecode_mnemonics[register]
+
+    else:
+      return "VARIABLE"
